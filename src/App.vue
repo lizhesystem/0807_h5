d5226710fc7e9664d1dce46f7b8ede481a193276
lz <lizhesystem@126.com>
2019-08-07 18:28:52 +0800

    0807 Off work

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
-    <div>
+    <div class="app-container">
+        <mt-header fixed title="固定在顶部"></mt-header>
+        <mt-button type="primary">primary</mt-button>
+        <mt-button plain>plain</mt-button>
+
         <h1>app组件</h1>
     </div>
 </template>
 
 <script>
-    export default {
-        name: "APP"
-    }
 </script>
 
 <style scoped>
+    .app-container {
+        padding-top: 40px;
+    }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,18 @@ import Vue from 'vue'
 // 导入路由
 import VueRouter from 'vue-router'
 
+// 导入mint-ui组件,不建议全部导入体积太大
+// 按需导入：先安装babel-plugin-component,然后在.babelrc加上官网说的配置加载css,如下
+// import Mint from 'mint-ui';
+// Vue.use(Mint);
+import { Header,Button  } from 'mint-ui';
+Vue.component(Header.name, Header);
+Vue.component(Button.name, Button);
+
+
 Vue.use(VueRouter);
 // 导入Vue的组件
-import app from './App.vue'
+import App from './App.vue'
 import routerObj from './router.js'
 
 // 创建vue对象
@@ -15,6 +24,7 @@ let vm = new Vue({
         return {}
     },
     methods: {},
-    render: c => c(app),
+    render: c => c(App),
     router: routerObj
+
 });
